08265073a524b9df4686d6ab82181fddf7b0d0f9
2003vinay <vinaymalyala2003@gmail.com>
2026-07-09 22:17:06 +0530

    Added application logging

--- app/app.py
+++ app/app.py
@@ -1,28 +1,40 @@
+import logging
 import os
 from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s %(message)s"
+)
+
+logger = logging.getLogger(__name__)
+
 APP_VERSION = os.getenv("APP_VERSION", "1.1.0")
 
 @app.route("/")
 def home():
+    logger.info("Home endpoint accessed")
     return "Welcome to Enterprise CI/CD Pipeline!"
 
 @app.route("/health")
 def health():
+    logger.info("Health endpoint accessed")
     return jsonify({
         "status": "UP"
     }), 200
 
 @app.route("/version")
 def version():
+    logger.info("Version endpoint accessed")
     return jsonify({
         "version": APP_VERSION
     }), 200
 
 @app.route("/info")
 def info():
+    logger.info("Info endpoint accessed")
     return jsonify({
         "application": "Enterprise CI/CD Pipeline",
         "version": APP_VERSION,
